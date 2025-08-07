20d71828fea98858c4abf758bac1002a65705718
tpals213 <tpals213@gmail.com>
2025-08-07 16:08:47 +0900

    faq 등록

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
-from app.api.endpoints import ads, ads_test, ads_notice, ads_user, ads_login, ads_app, ads_plan, ads_ticket, ads_push, ads_reserve
+from app.api.endpoints import (
+    ads, ads_test, ads_notice, ads_user, ads_login, ads_app, ads_plan, ads_ticket, ads_push, ads_reserve, ads_faq
+)
 from app.api.endpoints import webhook
 
 # 예약 설정
@@ -72,6 +74,7 @@ app.include_router(ads_plan.router, prefix="/plan")
 app.include_router(ads_ticket.router, prefix="/ticket")
 app.include_router(ads_push.router, prefix="/push")
 app.include_router(ads_reserve.router, prefix="/reserve")
+app.include_router(ads_faq.router, prefix="/faq")
 app.include_router(webhook.router, prefix="/ad")
 
 if __name__ == "__main__":
