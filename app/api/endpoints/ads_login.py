3fb3689ccd763ab42d222056cfd2e5b8989ee16c
tpals213 <tpals213@gmail.com>
2025-08-18 17:11:19 +0900

    업데이트 취소

--- app/api/endpoints/ads_login.py
+++ app/api/endpoints/ads_login.py
@@ -123,8 +123,8 @@ def ads_login_google_route(request: Google):
     user_id = service_get_user_by_provider(provider, google_id, email, request.device_token, request.installation_id)
     user_info = service_get_user_by_id(user_id)
 
-    if request.device_token:
-        service_update_device_token(user_id, request.device_token, request.android_id)
+    # if request.device_token:
+    #     service_update_device_token(user_id, request.device_token, request.android_id)
 
     # JWT 발급
     access_token = service_create_access_token(data={"sub": str(user_id)})
