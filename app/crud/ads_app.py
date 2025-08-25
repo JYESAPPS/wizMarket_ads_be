0c591d7d165290fb1bbb2c66605336da0f122f9f
DESKTOP-2RT8LJP\user <knany0911@gmail.com>
2025-08-25 17:45:10 +0900

    난영: upload_check 삭제

--- app/crud/ads_app.py
+++ app/crud/ads_app.py
@@ -140,8 +140,8 @@ def get_user_info(user_id):
 def get_user_record(user_id):
     try:
         connection = get_re_db_connection()
-        with connection.cursor(pymysql.cursors.DictCursor) as cursor:  # ✅ DictCursor 사용
-            cursor.execute("SELECT start_date, end_date, age, style, title, channel, upload_type, image_path FROM user_record WHERE user_id = %s and upload_check = 1", (user_id,))
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:  # ✅ DictCursor 사용 # and upload_check = 1 
+            cursor.execute("SELECT start_date, end_date, age, style, title, channel, upload_type, image_path FROM user_record WHERE user_id = %s", (user_id,))
             rows = cursor.fetchall()
 
         if not rows:
@@ -167,7 +167,6 @@ def get_user_record_this_month(user_id):
                 SELECT start_date, end_date, repeat_time, age, style, title, channel, image_path
                 FROM user_record
                 WHERE user_id = %s
-                AND upload_check = 1
             """
             cursor.execute(query, (user_id))
             rows = cursor.fetchall()
@@ -264,7 +263,6 @@ def get_user_recent_reco(request):
                 FROM user_record
                 WHERE user_id = %s 
                     AND upload_type = %s
-                    AND upload_check = 1
                     AND end_date >= CURDATE()  -- 오늘 포함 이후만
                 ORDER BY start_date DESC
             """, (user_id, upload_type))
